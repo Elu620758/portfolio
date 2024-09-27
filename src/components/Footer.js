053235e988da911c2b0aaea44c3ded5a68155616
Elu620758 <elumalai620758@gmail.com>
2024-09-27 19:29:15 +0530

    changed header and footer color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,3 @@
 export default function Footer(){
-    return <div className="py-4 bg-secondary text-center text-white">&copy; Elumalai P</div>
+    return <div className="py-4 bg-secondary text-center text-white" style={{backgroundColor:"#282C34"}}>&copy; Elumalai P</div>
 }
